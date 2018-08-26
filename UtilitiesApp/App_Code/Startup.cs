24384c646c836f0634fbbd8af9490a65c80e401f
Yordan <jjimenez1937@gmail.com>
2018-08-25 22:15:12 -0600

    Initial commit. New ASP Net Project is added.

---
+++ UtilitiesApp/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UtilitiesApp.Startup))]
+namespace UtilitiesApp
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
